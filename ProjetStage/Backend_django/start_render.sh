6a1baaffd42340e805c9d5e45876eacdfd0c332d
StageOrg Developer <dev@stageorg.com>
2026-07-29 17:55:49 +0000

    fix badrequest lors du login 6

---
+++ ProjetStage/Backend_django/start_render.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")"
+
+python manage.py migrate --noinput
+python create_admin.py
+
+exec gunicorn Backend_django.wsgi:application --bind 0.0.0.0:${PORT:-10000}
